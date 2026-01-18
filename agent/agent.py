a5e6433f687970b07330386031aca9bdd51c100b
shubgoenka19-dev <shub.goenka19@gmail.com>
2026-01-18 23:50:23 +0530

    Reformat agent.py for improved readability

--- agent/agent.py
+++ agent/agent.py
@@ -6,13 +6,14 @@ load_dotenv()
 from langchain_groq import ChatGroq
 from utils.config import MODEL_NAME
 
-# Safety check
+
 if not os.getenv("GROQ_API_KEY"):
     raise ValueError("GROQ_API_KEY not found. Check your .env file.")
 
-# Direct LLM (NO AGENT, NO TOOLS)
+
 llm = ChatGroq(
     model=MODEL_NAME,
     api_key=os.getenv("GROQ_API_KEY"),
     temperature=0.7
 )
+
